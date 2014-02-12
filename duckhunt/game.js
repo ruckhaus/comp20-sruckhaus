8a8e96a24df51a557ff9e1d2e43842289db81a4b
rucklehouse <sarah.ruckhaus@gmail.com>
2014-02-12 16:02:50 -0500

    add dirt, set up sprite sheet for slicing

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,15 @@
-// Your work goes here...
+// Sarah Ruckhaus's game.js
+
+function draw () {
+    var canvas = document.getElementById('game');
+    if (canvas.getContext) {
+	var ctx = canvas.getContext('2d');
+	ctx.fillStyle="#C96A1B";
+	ctx.fillRect(0,500,800,100);
+	var img = new Image();
+	img.addEventListener("load",function() {
+	    //Do drawImage stuff here?
+	}, false);
+	img.src = "/assets/duckhunt.png";
+    }
+}
