6c67500b41808dcc73b8ed0228ef47e47b7cd748
rucklehouse <sarah.ruckhaus@gmail.com>
2014-03-08 20:52:49 -0500

    resized signs

--- transit/transit.js
+++ transit/transit.js
@@ -80,9 +80,8 @@ function drawLine() {
 	}
 	stationMark = {
 		url: sign,
-		size: new google.maps.Size(10,20),
 		origin: new google.maps.Point(0,0),
-		anchor: new google.maps.Point(5,20)
+		anchor: new google.maps.Point(10,40)
 	};
 	j=0; //counter for marker array
 	linePath=[];
