51e690c12d8aa02fd7dcdf268ce402603caeb54f
rucklehouse <sarah.ruckhaus@gmail.com>
2014-03-09 17:13:23 -0400

    fixed info window content

--- transit/transit.js
+++ transit/transit.js
@@ -98,9 +98,9 @@ function drawLine() {
 			linePath[j] = tStop;
 			stopName[j] = stopMarker.title;
 			stopWindow = new google.maps.InfoWindow();
-			stopContent = '<h3>' + stopMarker.title + ' Station</h3>'
+			//stopContent = '<h3>' + stopMarker.title + ' Station</h3>'
 			google.maps.event.addListener(stopMarker, 'click', function() {
-				stopWindow.setContent(stopContent);
+				stopWindow.setContent('<h3>' + this.title + ' Station</h3>');
 				stopWindow.open(map, this);
 			});
 			j++;
