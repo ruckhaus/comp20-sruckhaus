dd080f6702ebcf334cb4aaecfc86951e74951cca
rucklehouse <sarah.ruckhaus@gmail.com>
2014-03-27 20:12:37 -0400

    add 2048 game and started app

---
+++ 2048-gamecenter/README.txt
@@ -0,0 +1,6 @@
+Sarah Ruckhaus
+COMP 20
+Assignment 4
+8 April 2014
+
+3/27: 10 min. Set up app.get and app.post statments.
---
+++ 2048-gamecenter/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express(express.logger());
+app.use(express.logger());
+
+//var mongo = require('mongodb');
+
+app.get('/', function (request, response) {
+
+});
+
+app.post('/submit.json', function (request, response) {
+
+});
+
+app.get('/scores.json'), function (request, response) {
+
+});
